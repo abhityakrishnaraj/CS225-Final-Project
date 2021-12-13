c32102a522d9d81e5d31e59f59846b337ccbe59c
Wei, Tony <tongwei3@illinois.edu>
2021-12-12 18:43:53 -0600

    Update shortestpath.cpp
    DFS and also minor bug changes to fix some issues

--- shortestpath.cpp
+++ shortestpath.cpp
@@ -56,6 +56,44 @@ vector<int> djikstra(int graph[V][V], int start, int goal){
     return path;
 }
 
+bool isValid(bool vis[][V], int row, int col) {
+    if (row < 0 || col < 0 || row >= V || col >= V)
+        return false;
+
+    if (vis[row][col]) {
+        return false;
+    }
+    return true;
+}
+
+void dfs(int row, int col, int grid[V][V], bool vis[][V]) {
+    int direction_row[] = { 0, 1, 0, -1 };
+    int direction_col[] = { -1, 0, 1, 0 };
+    
+    stack<pair<int, int> > s;
+    s.push({row, col});
+    
+    while (!s.empty()) {
+        pair<int, int> curr = s.top();
+        s.pop();
+        int r = curr.first;
+        int c = curr.second;
+
+        if (!isValid(vis, r, c)) {
+            continue;
+        }
+
+        vis[row][col] = true;
+        std::cout << grid[r][c] << " ";
+
+        for (int i = 0; i < V; i++) {
+            int x = r + direction_row[i];
+            int y = c + direction_col[i];
+            s.push({x, y});
+        }
+     }
+}
+
 int main(){
     int start = 0;
     int goal = 4;
